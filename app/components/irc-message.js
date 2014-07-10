d4b485af58b3b7b2033e62bf45d9117f88da1377
Omar Estrella <omar.estrella@inin.com>
2014-07-10 17:26:21 -0400

    Show image links

--- app/components/irc-message.js
+++ app/components/irc-message.js
@@ -3,7 +3,34 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     classNames: ['message'],
 
+    imageUrls: null,
+
+    setPerMessageProperties: function () {
+        this.set('imageUrls', []);
+    }.on('init'),
+
     linkifyText: function () {
         this.$('.text').linkify();
-    }.on('didInsertElement')
+        var links = this.$('a');
+        this.collectImageUrls(links);
+    }.on('didInsertElement'),
+
+    collectImageUrls: function (links) {
+        var self = this;
+
+        links.each(function (index, link) {
+            var url = Ember.$(this).attr('href');
+
+            Ember.$.ajax({
+                type: 'head',
+                url: url,
+                success: function (data, status, xhr) {
+                    var contentType = xhr.getResponseHeader('Content-Type');
+                    if (contentType.indexOf('image') > -1) {
+                        self.get('imageUrls').addObject(url);
+                    }
+                }
+            });
+        });
+    }
 });
